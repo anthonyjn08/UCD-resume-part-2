880cf6b92b7f1e09792e4f9a8652b9fa3e1aeeb5
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-05-15 17:22:14 +0000

    Implement emailjs integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+
+    var templateParams = {
+        from_name: contactForm.name.value,
+        from_email: contactForm.emailaddress.value,
+        project_request: contactForm.projectsummary.value
+    };
+    
+    emailjs.send("outlook", "rosie", templateParams)
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
